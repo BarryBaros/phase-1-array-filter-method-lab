6f543578a084a8a381d0b356a1226384aa886be0
Timothy Barasa <timbaros87@gmail.com>
2024-04-04 15:25:38 +0300

    use filter() to return matching results.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution here
+const drivers = ["Albert", "Michael", "Joseph", "William", "Henry"]
+
+// findMatching function
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+console.log(findMatching(drivers, 'Albert'));
+
+
+
+// fuzzyMatch function
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+console.log(fuzzyMatch(drivers, 'Mi'));
+console.log(fuzzyMatch(drivers, 'Jo'));
+console.log(fuzzyMatch(drivers, 'Wi'));
+
+
+
+
+const matchDrivers = [ 
+    {
+        name: "William",
+        age: 32,
+        address: "Nairobi"
+    },
+    {
+        name: "Joseph",
+        age: 28,
+        address: "Mombasa"
+    },
+    {
+        name: "Albert",
+        age: 36,
+        address: "Kisumu"
+    },
+    {
+        name: "Michael",
+        age: 39,
+        address: "Nakuru"
+    }
+];
+// matchName function
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+// console.log(matchName(drivers, 'Michael'));
+
